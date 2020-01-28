5880ec5a75868d9c463522626634630f1fef2899
ahaavisto <aino.r.haavisto@helsinki.fi>
2020-01-28 21:10:12 +0200

    luotu testiluokka

---
+++ kastaaja/src/test/java/TestKastaaja.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author arkkis
+ */
+
+import ahaavisto.kastaaja.Kastaaja
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class TestKastaaja {
+    @Test
+    public void testNothing() {
+        assertTrue(true);
+    }
+}
